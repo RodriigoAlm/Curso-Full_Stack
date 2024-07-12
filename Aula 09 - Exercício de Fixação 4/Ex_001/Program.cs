9ccedcbf43092b95dd3a975aee248cc80089992b
Rodrigo Almeida <rodriigoalm23@gmail.com>
2024-07-12 14:37:42 -0300

    Adicionando ex_01 - aula 09

---
+++ Aula 09 - Exercício de Fixação 4/Ex_001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Digite uma nota de 0 a 10: ");
+byte nota = byte.Parse(Console.ReadLine());
+
+while (nota > 10) {
+    Console.WriteLine("A nota digitada é maior que 10, digite novamente.");
+    Console.Write("Digite uma nota de 0 a 10: ");
+    nota = byte.Parse(Console.ReadLine());
+};
+Console.WriteLine($"A nota digitada foi: {nota}");
