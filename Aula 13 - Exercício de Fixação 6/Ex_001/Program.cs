82b0a865f3515527968430c1d148156e7c6bbb13
Rodrigo Almeida <rodriigoalm23@gmail.com>
2024-08-01 10:28:00 -0300

    Adicionando Exercícios

---
+++ Aula 13 - Exercício de Fixação 6/Ex_001/Program.cs
@@ -0,0 +1,22 @@
+﻿int coluna = 0;
+int linha = 0;
+int maior = 0;
+
+int[,] numeros = new int[4,4] {
+    {1,56,12,45},
+    {10,11,9,25},
+    {120,22,89,5},
+    {57,3,67,43}
+};
+
+for (int i = 0; i < numeros.GetLength(0); i++) {
+    for (int c = 0; c < numeros.GetLength(1); c++) {
+        if (numeros[i,c] > maior) {
+            maior = numeros[i,c];
+            linha = i;
+            coluna = c;
+        }
+    }
+}
+
+Console.WriteLine($"O maior número da matriz é: {maior}. Está na posição [{linha},{coluna}]");
